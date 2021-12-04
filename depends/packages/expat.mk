c9f3f04569487e00a9b1cf8288092f0c66e56146
sredouane <semghouni.redouane@gmail.com>
2021-12-04 13:30:42 -0500

    ubuntu build

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.2.1
+$(package)_version=2.4.1
 $(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/$($(package)_version)
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=1868cadae4c82a018e361e2b2091de103cd820aaacb0d6cfa49bd2cd83978885
+$(package)_sha256_hash=2F9B6A580B94577B150A7D5617AD4643A4301A6616FF459307DF3E225BCFBF40
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-static
